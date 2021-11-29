fd0f9f54be8e9c52d602099027a8f60c48d45dfa
Bogdan Gaza <bc.gaza@gmail.com>
2021-11-28 20:07:14 -0800

    [dotfiles] Updated config.fish with proper paths for M1 homebrew.

--- fish/config.fish
+++ fish/config.fish
@@ -1,25 +1,15 @@
 # Set default editor
 set -x EDITOR (which vim)
 # Set paths needed for homebrew
-set -x PATH /usr/local/bin $PATH
-set -x PATH /usr/local/sbin $PATH
+set -x PATH /opt/homebrew/bin $PATH
+set -x PATH /opt/homebrew/sbin $PATH
+
+status --is-interactive; and source (rbenv init -|psub)
 
 set fish_greeting ""
 
-set -x GOPATH $HOME/.golang:$GOPATH
-
-set -x PATH $HOME/.golang/bin $PATH
-
 set -x PATH /Applications/Postgres.app/Contents/Versions/latest/bin $PATH
 
-set -gx PATH "$HOME/.cargo/bin" $PATH;
-
 # The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/bogdan/Projects/google-cloud-sdk/path.fish.inc' ]; if type source > /dev/null; source '/Users/bogdan/Projects/google-cloud-sdk/path.fish.inc'; else; . '/Users/bogdan/Projects/google-cloud-sdk/path.fish.inc'; end; end
-set -g fish_user_paths "/usr/local/opt/openssl/bin" $fish_user_paths
-
-# openssl flags for the compilers to find
-set -gx LDFLAGS "-L/usr/local/opt/openssl/lib"
-set -gx CPPFLAGS "-I/usr/local/opt/openssl/include"
-
-status --is-interactive; and source (rbenv init -|psub)
+if [ -f "$HOME/Projects/misc/google-cloud-sdk/path.fish.inc" ]; . "$HOME/Projects/misc/google-cloud-sdk/path.fish.inc"; end
+fish_add_path /opt/homebrew/opt/node@14/bin
