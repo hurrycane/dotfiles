e8bbeb975d0b39e6d9c2acd1d53b61e9041f70a1
Bogdan Gaza <bc.gaza@gmail.com>
2026-07-25 09:18:24 -0700

    Cleaning up fish config

--- fish/config.fish
+++ fish/config.fish
@@ -5,8 +5,16 @@ set -x PATH /opt/homebrew/bin $PATH
 set -x PATH /opt/homebrew/sbin $PATH
 set -x PATH /opt/homebrew/lib $PATH
 
-# status --is-interactive; and ~/.rbenv/bin/rbenv init - fish | source
-
 set fish_greeting ""
 
+fish_add_path /Users/bogdan/Downloads/hyperpodeks/bin
+
+fish_add_path /opt/homebrew/opt/llvm/bin
+
+set -gx LDFLAGS "-L/opt/homebrew/opt/llvm/lib"
+set -gx CPPFLAGS "-I/opt/homebrew/opt/llvm/include"
+
 starship init fish | source
+direnv hook fish | source
+
+source $HOME/.local/bin/env.fish
